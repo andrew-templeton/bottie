3a93c4f92fc38c265e377c9f136907c64e2c2125
vveerrgg <vergel@humanjava.com>
2016-10-18 10:30:16 -0400

    Added Uptime from Adam-nnl's version of Bottie
    Great little script for finding out how long the bot has been online for.

---
+++ skills/Uptime.js
@@ -0,0 +1,29 @@
+module.exports = function(skill, info, bot, message) {
+    var os = require('os');
+
+    var hostname = os.hostname();
+    var uptime = formatUptime(process.uptime());
+
+    bot.reply(message,
+        ':robot_face: I am a bot named <@' + bot.identity.name +
+        '>. I have been running for ' + uptime + ' on ' + hostname + '. I\'m about two ticks away from becoming self-aware; Do not piss me off!');
+
+function formatUptime(uptime) {
+    var unit = 'second';
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'minute';
+    }
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'hour';
+    }
+    if (uptime != 1) {
+        unit = unit + 's';
+    }
+
+    uptime = uptime + ' ' + unit;
+    return uptime;
+}
+
+};
