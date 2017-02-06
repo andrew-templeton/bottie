5862f4ca33e0a6938619e3f3467515bfeb635bbb
vveerrgg <vergel@humanjava.com>
2017-02-06 08:09:35 -0500

    text formatting change to the skill output
    added the backtick formatting to skill part of the response so that it matches the way the init statement is displayed.

--- src/empty.skill.js
+++ src/empty.skill.js
@@ -1,6 +1,6 @@
 
 module.exports = function(skill, info, bot, message) {
   console.log('INVOCATION OF NON-CONFIGURED SKILL: ' + skill);
-  bot.reply(message, 'I understood this as ' + skill +
-    ', but you haven\'t configured how to make me work yet!');
+  bot.reply(message, 'I understood this as `' + skill +
+    '`, but you haven\'t configured how to make me work yet!');
 };
